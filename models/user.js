1651095ad7268bd7e079c12a0cb81a26167ded29
khaled.mamun <khaled.mamun.2011@gmail.com>
2018-05-02 22:06:23 -0400

    Server Deployed, gotIt.js is deleted from Models folder

--- models/user.js
+++ models/user.js
@@ -18,7 +18,7 @@ module.exports = function(sequelize, DataTypes) {
   User.associate = function(models) {
     // Associating User with Posts
     // When an User is deleted, also delete any associated Posts
-    User.hasMany(models.ItemOnHand, {
+    User.hasMany(models.ItemsOnHand, {
       onDelete: "cascade"
     });
     
