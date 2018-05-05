1933f4c8594c0a01c1e53131e015255379565efe
khaled.mamun <khaled.mamun.2011@gmail.com>
2018-05-05 16:32:43 -0400

    Adding Routes

---
+++ routes/items-api-routes.js
@@ -0,0 +1,75 @@
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+
+// Requiring our models
+var db = require("../models");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  // GET route for getting all of the ItemList
+  app.get("/api/items", function(req, res) {
+    var query = {};
+    if (req.query.user_id) {
+      query.UserId = req.query.user_id;
+    }
+    // Here we add an "include" property to our options in our findAll query
+    // We set the value to an array of the models we want to include in a left outer join
+    // In this case, just db.Author
+    db.ItemList.findAll({
+      where: query,
+      include: [db.User]
+    }).then(function(dbItems) {
+      res.json(dbItems);
+    });
+  });
+
+  // Get route for retrieving a single ItemList
+  app.get("/api/items/:id", function(req, res) {
+   
+    db.ItemList.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [db.User]
+    }).then(function(dbItems) {
+      res.json(dbItems);
+    });
+  });
+
+  // ItemList route for saving a new ItemList
+  app.post("/api/items", function(req, res) {
+    db.ItemList.create(req.body).then(function(dbItems) {
+      res.json(dbItems);
+    });
+  });
+
+  // DELETE route for deleting ItemList
+  app.delete("/api/items/:id", function(req, res) {
+    db.ItemList.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbItems) {
+      res.json(dbItems);
+    });
+  });
+
+  // PUT route for updating ItemList
+  app.put("/api/items", function(req, res) {
+    db.ItemList.update(
+      req.body,
+      {
+        where: {
+          id: req.body.id
+        }
+      }).then(function(dbItems) {
+      res.json(dbItems);
+    });
+  });
+};
--- server.js
+++ server.js
@@ -28,9 +28,11 @@ app.use(express.static("public"));
 
 // Routes
 // =============================================================
-// require("./routes/html-routes.js")(app);
-// require("./routes/author-api-routes.js")(app);
-// require("./routes/post-api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+require("./routes/categories-api-routes.js")(app);
+require("./routes/items-api-routes.js")(app);
+require("./routes/users-api-routes.js")(app);
+require("./routes/wishes-api-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
