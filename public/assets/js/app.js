05cc832cbc006ad6a3cba4333149759c23f51582
nh nc <nhnc11@yahoo.com>
2018-05-07 20:44:37 -0400

    middle

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 $(document).ready(function(){
     $('#radio-buttons').hide();
     $('#newItem').hide();
@@ -61,3 +62,49 @@ $(document).ready(function(){
 // $("#changeBudget").click(function() {
 //     $("#formToChangeBudget").toggle();
 // });
+=======
+$("#boughtItem").click(function() {
+    $('#findItemInfo').toggle();
+});
+
+$("#wishList").click(function() {
+    $('#neededList').toggle();
+});
+
+$("#addMember").click(function() {
+    $('#formToAddMember').toggle();
+});
+
+$("#changeBudget").click(function() {
+    $('#formToChangeBudget').toggle();
+});
+
+    for (var i = 0; i < memberNum.length; i++) {
+        var option_i =  $("<option>" + memberNum[i] + "</option>");
+        option_i.attr("data-value", memberNum[i]);
+        $(".nameofUser").append(option_i);
+    }
+}
+
+     $("form-submit").on("submit", function(event) {
+         // Make sure to preventDefault on a submit event.
+         event.preventDefault();
+
+         var newUser = {
+             user_name: $("#name").val().trim(),
+
+         };
+
+         // Send the POST request.
+         $.ajax("/", {
+             type: "POST",
+             data: newUser
+         }).then(
+             function() {
+                 console.log("created new user");
+                 // Reload the page to get the updated list
+                 location.reload();
+             }
+         );
+     });
+>>>>>>> middle
