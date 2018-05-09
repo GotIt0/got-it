e3cbe23023340798d4fe48df9cc87fb78f5cc1f0
noorehtesham <noore44@vt.edu>
2018-05-09 18:21:53 -0400

    more front end chages

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,32 +1,40 @@
 $(document).ready(function(){
-    $('#radio-buttons').hide();
-    $('#newItem').hide();
-    $('#formToAddMember').hide();
-    $('#formToChangeBudget').hide();
-    $('#dollar-amount').hide();
-    $('.card').hide();
-    $('#neededList').hide();
-    $("#addAnItem").hide();
+
+
+    
+    
 
     $("#boughtIitem").click(function() {
-        $("#addAnItem").toggle();
-        console.log("im here");
+        // $('.cd-fixed-bg.cd-fixed-bg--1').toggle();
+        // console.log("im here");
+        // $('.cd-fixed-bg.cd-fixed-bg--7').show();
+        window.location.href= '#radio-buttons';
     });
    
     $("#wishList").click(function() {
-        $("#neededList").toggle();
+        // $('.cd-fixed-bg.cd-fixed-bg--2').toggle();
+        // $('.cd-fixed-bg.cd-fixed-bg--5').show();
+        window.location.href= '#neededList';
     });
     
     $("#addMember").click(function() {
-        $("#formToAddMember").toggle();
+        // $('.cd-fixed-bg.cd-fixed-bg--3').toggle();
+        // console.log("IM here")
+        // $('.cd-fixed-bg.cd-fixed-bg--6').show();
+        window.location.href='#formToAddMember';
     });
+
     
     $("#changeBudget").click(function() {
-        $("#formToChangeBudget").toggle();
-        console.log("im here");
+        window.location.href='#formToChangeBudget';
+    
         
     });
 })
+
+if($('#gridRadios1').is(':checked')) {
+    $('.nameofUser').append()
+}
 
 // // $("#boughtItem").click(function() {
 // //     $('#findItemInfo').show();
