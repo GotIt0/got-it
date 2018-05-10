e2b1651e399990bf1483df82d9833958c13e6b2d
khaled.mamun <khaled.mamun.2011@gmail.com>
2018-05-09 20:57:15 -0400

    Event Handeler Updated

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -28,34 +28,6 @@ $(document).ready(function(){
         
     });
 
-        for (var i = 0; i < memberNum.length; i++) {
-        var option_i =  $("<option>" + memberNum[i] + "</option>");
-        option_i.attr("data-value", memberNum[i]);
-        $(".nameofUser").append(option_i);
-    }
-
-
-     $("form-submit").on("submit", function(event) {
-         // Make sure to preventDefault on a submit event.
-         event.preventDefault();
-
-         var newUser = {
-             user_name: $("#name").val().trim(),
-
-         };
-
-         // Send the POST request.
-         $.ajax("/", {
-             type: "POST",
-             data: newUser
-         }).then(
-             function() {
-                 console.log("created new user");
-                 // Reload the page to get the updated list
-                 location.reload();
-             }
-         );
-     });
 })
 
 
